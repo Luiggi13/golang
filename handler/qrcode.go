4ccb4a03f3e2c7c64e571237263b28f87dce5a01
Christian <christian.llansola@gmail.com>
2024-09-01 14:13:41 +0200

    Refactor: GenerateQrCode to utils package

--- handler/qrcode.go
+++ handler/qrcode.go
@@ -1,12 +1,10 @@
 package handler
 
 import (
-	"encoding/base64"
 	modelQR "goapi/models"
 	u "goapi/utils"
 
 	"github.com/gofiber/fiber/v2"
-	"github.com/skip2/go-qrcode"
 )
 
 func CreateQrCode(c *fiber.Ctx) interface{} {
@@ -20,13 +18,5 @@ func CreateQrCode(c *fiber.Ctx) interface{} {
 		return c.Status(fiber.StatusBadRequest).JSON(modelQR.ApiBadRequest)
 	}
 
-	png, errEncode := qrcode.Encode(inputUrl.URLString, qrcode.Highest, 1024)
-	if errEncode != nil {
-		defaultQrCode, _ := qrcode.Encode("https://www.default-url.com", qrcode.Highest, 1024)
-		encoded := base64.StdEncoding.EncodeToString(defaultQrCode)
-		return encoded
-	} else {
-		encoded := base64.StdEncoding.EncodeToString(png)
-		return encoded
-	}
+	return u.GenerateQrCode(inputUrl.URLString)
 }
--- utils/utils.go
+++ utils/utils.go
@@ -1,9 +1,14 @@
 package utils
 
 import (
+	"encoding/base64"
 	"fmt"
 	"net/url"
+
+	"github.com/skip2/go-qrcode"
 )
+
+const DEFAULTURL = "https://www.default-url.com"
 
 func ValidateURL(urlString string) error {
 	u, err := url.ParseRequestURI(urlString)
@@ -11,4 +16,16 @@ func ValidateURL(urlString string) error {
 		return fmt.Errorf("invalid URL")
 	}
 	return nil
+}
+
+func GenerateQrCode(url string) string {
+	png, errEncode := qrcode.Encode(url, qrcode.Highest, 1024)
+	if errEncode != nil {
+		defaultQrCode, _ := qrcode.Encode(DEFAULTURL, qrcode.Highest, 1024)
+		encoded := base64.StdEncoding.EncodeToString(defaultQrCode)
+		return encoded
+	} else {
+		encoded := base64.StdEncoding.EncodeToString(png)
+		return encoded
+	}
 }
