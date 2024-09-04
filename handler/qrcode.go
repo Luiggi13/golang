2763761832f4f93c3275c360cf81c43affeedb33
Christian <christian.llansola@gmail.com>
2024-09-04 15:25:41 +0200

    Refactor and docs: If url is not provided, it returns a default qrcode

--- handler/qrcode.go
+++ handler/qrcode.go
@@ -13,7 +13,6 @@ import (
 // The function performs the following steps:
 // 1. Decodes the request body into a QrInput struct (var inputUrl m.QrInput).
 // 2. Checks if the request body contains a premium field and sets the isPremium variable accordingly.
-// 3. Validates the URL provided in the request body using the ValidateURL function from the utils package.
 // 4. Checks if the user is authenticated using the IsUserProvided function from the utils package.
 // 5. If any validation fails, it returns a 400 Bad Request response with an appropriate error message.
 // 6. If the user is authenticated and the URL is valid, it generates a QR code using the GenerateQrCode function from the utils package.
@@ -29,10 +28,6 @@ func CreateQrCode(c *fiber.Ctx) interface{} {
 
 	if !u.IsUserProvided(inputUrl, c.Method()) {
 		return m.BadRequestError(m.BaseError{Message: "Error user", Method: c.Method()})
-	}
-
-	if err := u.ValidateURL(inputUrl.URLString); err != nil {
-		return m.BadRequestError(m.BaseError{Message: "Error user", Method: c.Method(), Url: inputUrl.URLString})
 	}
 
 	if inputUrl.Premium != nil {
