f4274de1c5ce76863944bcbe4fe78342553461d4
Luiggi13 <christian.llansola@gmail.com>
2024-09-06 21:12:15 +0200

    Feature for admin users: User, tags table, migration, seeders (#10)

--- database/sqls/structure.sql
+++ database/sqls/structure.sql
@@ -6,3 +6,19 @@ CREATE TABLE IF NOT EXISTS qrs (
   premium bool DEFAULT false null,
   created_at timestamp DEFAULT now()
 );
+
+create table users (
+  id uuid primary key not null,
+  name text,
+  premium bool default false,
+  email text,
+  created_at timestamp with time zone default now()
+);
+
+create table tags (
+  id uuid primary key not null,
+  name text,
+  public boolean,
+  qrid bigint references qrs (id),
+  iduser uuid references users (id)
+);
--- handler/health.go
+++ handler/health.go
@@ -21,10 +21,14 @@ func GetHealth(c *fiber.Ctx) Health {
 }
 
 func InitMigration(c *fiber.Ctx) interface{} {
-	db.Connect_db(true, false)
+	db.Connect_db(true, false, false)
 	return m.MigrationInterface(m.BaseError{Message: "Migration deployed correctly", Method: c.Method()})
 }
 func InitSeeders(c *fiber.Ctx) interface{} {
-	db.Connect_db(false, true)
+	db.Connect_db(false, true, false)
 	return m.MigrationInterface(m.BaseError{Message: "Seeder deployed correctly", Method: c.Method()})
+}
+func CleanTables(c *fiber.Ctx) interface{} {
+	// db.Connect_db(false, true, true)
+	return m.MigrationInterface(m.BaseError{Message: "Cleaned tables correctly", Method: c.Method()})
 }
