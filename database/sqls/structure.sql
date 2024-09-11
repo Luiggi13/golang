f417b526e779cb1bae6dd243e95a4a026a5c2c85
Christian <christian.llansola@gmail.com>
2024-09-11 15:12:27 +0200

    SQL's updated accordingly

--- database/sqls/structure.sql
+++ database/sqls/structure.sql
@@ -1,3 +1,12 @@
+create TABLE users (
+  id bigint  primary key generated always as identity,
+  name text,
+  premium bool default false,
+  email text,
+  active bool default true,
+  created_at timestamp with time zone default now()
+);
+
 create table tags (
   id bigint primary key generated always as identity,
   name text,
@@ -7,9 +16,10 @@ create table tags (
 CREATE TABLE IF NOT EXISTS qrs (
   id bigint primary key generated always as identity,
   qr_code text,
-  userId text,
   url_text text,
   premium bool DEFAULT false null,
   id_tag bigint references tags (id) ON DELETE SET NULL,
+  id_user bigint references users (id) ON DELETE SET NULL,
   created_at timestamp DEFAULT now()
 );
+
